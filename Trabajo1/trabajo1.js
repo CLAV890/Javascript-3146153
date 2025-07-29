ef964dfe324ee7ab205371151c27d1d627a46a28
CLAV890 <cmilo890@outlook.com>
2025-07-29 15:39:36 -0500

    APLICACION FOTO PARTE 1

---
+++ Trabajo1/trabajo1.js
@@ -0,0 +1,4 @@
+let imagenMario = document.getElementById("imagenMraio");
+let aplicarFiltroBlancoNegro = document.getElementById("aplicarFiltroBlancoNegro");
+let aplicarDesenfoque = document.getElementById("aplicarDesenfoque");
+let aplicarZoomYRotacion = document.getElementById("aplicarZoomYRotacion");
---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Hola Marlon");
+let name = "Camilo"
+if ( 5 == 5 ) {
+    console.log("Bienvenido")
+}
+
+
+ 
