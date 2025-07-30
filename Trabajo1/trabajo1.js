dcd6b1715db1f906ab1cd9bf8e4f7b4c61994890
CLAV890 <cmilo890@outlook.com>
2025-07-30 15:34:57 -0500

    APLICACION TERMINADA

--- Trabajo1/trabajo1.js
+++ Trabajo1/trabajo1.js
@@ -1,4 +1,32 @@
-let imagenMario = document.getElementById("imagenMraio");
-let aplicarFiltroBlancoNegro = document.getElementById("aplicarFiltroBlancoNegro");
-let aplicarDesenfoque = document.getElementById("aplicarDesenfoque");
-let aplicarZoomYRotacion = document.getElementById("aplicarZoomYRotacion");
+const imagenMario= document.querySelector("#imagenMario");
+
+const Btn1= document.querySelector("#Btn1")
+const Btn2= document.querySelector("#Btn2")
+const Btn3= document.querySelector("#Btn3")
+
+
+function blancoynegro () {
+    console.log("Imagen modificada")
+    imagenMario.style.filter = "grayscale(100%)"
+
+}
+function desenfoque () {
+    imagenMario.style.filter = "blur(5px)";
+}
+
+
+function rotacionyzoom () {
+    imagenMario.style.transform = "scale(1.5)";
+
+    imagenMario.style.transform= "rotate(45deg)";
+
+}
+
+
+
+
+
+
+Btn1.addEventListener("click", blancoynegro);
+Btn2.addEventListener("click", desenfoque);
+Btn3.addEventListener("click", rotacionyzoom);
