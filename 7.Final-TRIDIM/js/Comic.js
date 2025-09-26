3b8734182c4eadde95d6ad3b801e209050943007
CLAV890 <cmilo890@outlook.com>
2025-09-26 14:52:50 -0500

    Avance

--- 7.Final-TRIDIM/js/Comic.js
+++ 7.Final-TRIDIM/js/Comic.js
@@ -1,10 +1,22 @@
 
-
 const tituloComic = document.querySelector(".titulo-comic");
 const descripcionComic = document.querySelector(".Descripcion-comic");
-const imgComic = document.querySelector(".img-comic");
 const listaCaps = document.querySelector(".lista-caps");
+const video = document.querySelector(".video-play");  
+const btnPlay = document.querySelector(".play");     
 
+
+function toggleVideo() {
+  if (video.paused) {
+    video.play();
+    btnPlay.textContent = "⏸ Stop";
+  } else {
+    video.pause();
+    btnPlay.textContent = "▶ Play";
+  }
+}
+
+btnPlay.addEventListener("click", toggleVideo);
 
 tituloComic.textContent = comic.nombreComic;
 descripcionComic.textContent = comic.categoria;
@@ -16,7 +28,7 @@ listaCaps.innerHTML = "";
 comic.capitulos.forEach(cap => {
   const li = document.createElement("li");
   li.innerHTML = `
-  <a href="./Comic.html?id=${cap.id}">
+  <a href="./.html?id=${cap.id}">
     <img class="imgC"  src="${cap.imPortada}" alt="${cap.nombreCap}">
     <p>${cap.nombreCap}</p>
   `;
--- 7.Final-TRIDIM/js/infocomicbd.js
+++ 7.Final-TRIDIM/js/infocomicbd.js
@@ -1,41 +1,46 @@
 const comic = {
 
-    "nombreComic" : "TRIDIM",
+    "nombreComic": "TRIDIM",
     "categoria": "Viaje1",
-    "cantidadCap" : 5,
-    "descripcion" : "Trata de una crisis economica",
-    "personajes" : ["pepa", "lucy","mario"],
+    "cantidadCap": 5,
+    "descripcion": "Trata de una crisis economica",
+    "personajes": ["DIMITRI", "TRIDIM", "IGNIS"],
     "capitulos": [
         {
+            id: 1,
             nombreCap: "CAPITULO 1",
             imPortada: "../Sources/assets/Capitulo1TRIDIM.JPG",
-            descripcion: "",
-            personajes: ["TRIDIM","DIMITRI","IGNIS"]
-            
+            descripcion: "Trata de una crisis economica",
+            personajes: ["TRIDIM", "DIMITRI", "IGNIS"]
+
         },
         {
+            id: 2,
             nombreCap: "CAPITULO 2",
             imPortada: "../Sources/assets/Capitulo2TRIDIM.PNG",
-            descripcion: "Lorem imps",
-            personajes: ["pepa","luci","mario"]
+            descripcion: "Trata de una crisis economica",
+            personajes: ["pepa", "luci", "mario"]
         },
         {
+            id: 3,
             nombreCap: "CAPITULO 3",
             imPortada: "../Sources/assets/Capitulo1TRIDIM.JPG",
             descripcion: "Lorem imps",
-            personajes: ["pepa","luci","mario"]
+            personajes: ["pepa", "luci", "mario"]
         },
         {
+            id: 4,
             nombreCap: "CAPITULO 4",
             imPortada: "../Sources/assets/Capitulo2TRIDIM.PNG",
             descripcion: "Lorem imps",
-            personajes: ["pepa","luci","mario"]
+            personajes: ["pepa", "luci", "mario"]
         },
         {
+            id: 5,
             nombreCap: "CAPITULO 5",
             imPortada: "../Sources/assets/Capitulo1TRIDIM.JPG",
             descripcion: "Lorem imps",
-            personajes: ["pepa","luci","mario"]
+            personajes: ["pepa", "luci", "mario"]
         }
     ]
 }
